9743638fd350eb28aea135e606d86754e73c7279
kyungsikjeung <datamining7830@gmail.com>
2020-04-28 19:28:32 +0900

    에러 수정

--- front-end/src/services/registration/index.js
+++ front-end/src/services/registration/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default{
   register(details){
-    return new Promise((resolve,reject)=>{axios.post('/registrations',details).then({data}=>resolve(data)).catch(error){
+    return new Promise((resolve,reject)=>{axios.post('/registrations',details).then((data)=>resolve(data)).catch(error){
       reject(error)
     }})
   }
